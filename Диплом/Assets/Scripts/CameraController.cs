ec862cd1201e2727a1f19d738dbb7464678f3576
Arv1L <vovakibets88@gmail.com>
2022-03-28 21:09:21 +0300

    m03d28

--- Диплом/Assets/Scripts/CameraController.cs
+++ Диплом/Assets/Scripts/CameraController.cs
@@ -3,80 +3,43 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public float zoomSpeed;
-    private float step = 0.5f;
     public float moveDistance;
+    private Camera MainCamera;
 
     private void Start()
     {
+        MainCamera = GetComponent<Camera>();
     }
 
-    void CameraMove(int minValue, int maxValue)
+    void CameraMove(int minCorner, int maxCorner)
     {
-        if (transform.position.x >= minValue && transform.position.z >= minValue && transform.position.x <= maxValue && transform.position.z <= maxValue)
+        if (transform.position.x >= minCorner && transform.position.z >= minCorner && transform.position.x <= maxCorner && transform.position.z <= maxCorner)
         {
             if (Input.GetKey(KeyCode.W))
-            {
-                transform.position = new Vector3(transform.position.x + (Time.deltaTime * moveDistance), transform.position.y, transform.position.z + (Time.deltaTime * moveDistance));
-            }
+                transform.position = new Vector3(transform.position.x + Time.deltaTime * moveDistance, transform.position.y, transform.position.z + Time.deltaTime * moveDistance);
             if (Input.GetKey(KeyCode.S))
-            {
-                transform.position = new Vector3(transform.position.x - (Time.deltaTime * moveDistance), transform.position.y, transform.position.z - (Time.deltaTime * moveDistance));
-            }
+                transform.position = new Vector3(transform.position.x - Time.deltaTime * moveDistance, transform.position.y, transform.position.z - Time.deltaTime * moveDistance);
             if (Input.GetKey(KeyCode.A))
-            {
-                transform.position = new Vector3(transform.position.x - (Time.deltaTime * moveDistance), transform.position.y, transform.position.z + (Time.deltaTime * moveDistance));
-            }
+                transform.position = new Vector3(transform.position.x - Time.deltaTime * moveDistance, transform.position.y, transform.position.z + Time.deltaTime * moveDistance);
             if (Input.GetKey(KeyCode.D))
-            {
-                transform.position = new Vector3(transform.position.x + (Time.deltaTime * moveDistance), transform.position.y, transform.position.z - (Time.deltaTime * moveDistance));
-            }
+                transform.position = new Vector3(transform.position.x + Time.deltaTime * moveDistance, transform.position.y, transform.position.z - Time.deltaTime * moveDistance);
         }
-        else if (transform.position.x < minValue)
-        {
-            transform.position = new Vector3(transform.position.x + (Time.deltaTime * moveDistance), transform.position.y, transform.position.z);
-        }
-        else if (transform.position.z < minValue)
-        {
-            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + (Time.deltaTime * moveDistance));
-        }
-        else if (transform.position.x > maxValue)
-        {
-            transform.position = new Vector3(transform.position.x - (Time.deltaTime * moveDistance), transform.position.y, transform.position.z);
-        }
-        else if (transform.position.z > maxValue)
-        {
-            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - (Time.deltaTime * moveDistance));
-        }
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, minCorner, maxCorner), transform.position.y, Mathf.Clamp(transform.localPosition.z, minCorner, maxCorner));
     }
 
     void Update()
     {
-        float scroll = Input.GetAxis("Mouse ScrollWheel");
-        
-        if (transform.position.y >= 3 && transform.position.y <= 10)
-        {
-            transform.position += transform.forward * scroll * zoomSpeed;
-        }
-        else if (transform.position.y < 3)
-        {
-            transform.position = new Vector3(transform.position.x - step, 3, transform.position.z - step);
-        }
-        else if (transform.position.y > 10)
-        {
-            transform.position = new Vector3(transform.position.x + step, 10, transform.position.z + step);
-        }
+        int minSize = 1;
+        int maxSize = 4;
 
-        if (transform.position.y <= 4)
-        {
-            int minValue = -8;
-            int maxValue = 5;
-            CameraMove(minValue, maxValue);
-        }
-        else if (transform.position.y > 4)
-        {
-            int minValue = -10;
-            int maxValue = 0;
-            CameraMove(minValue, maxValue);
-        }
+        int minCorner = -10;
+        int maxCorner = 3;
+
+        if (Input.mouseScrollDelta.y != 0 && MainCamera.orthographicSize >= minSize && MainCamera.orthographicSize <= maxSize) 
+            MainCamera.orthographicSize -= Input.mouseScrollDelta.y * zoomSpeed * Time.deltaTime;
+        else
+            MainCamera.orthographicSize = Mathf.Clamp(MainCamera.orthographicSize, minSize, maxSize);
+
+        CameraMove(minCorner, maxCorner);
     }
 }
