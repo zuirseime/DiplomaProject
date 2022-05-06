131cb79d17ae8fbbd78809af62c3a1a72500eaee
ForesterBear <102464937+ForesterBear@users.noreply.github.com>
2022-05-06 19:52:00 +0300

    m05d06

---
+++ Диплом/Assets/Scripts/OptionsMenu.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class OptionsMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("volume", Mathf.Log10(volume)*20);
+    }
+
+    public void Sound()
+    {
+        AudioListener.pause = !AudioListener.pause;
+    }
+
+
+
+}
