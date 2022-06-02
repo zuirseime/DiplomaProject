4f93d7df30b510989c423d5707f748c25d4729ac
ForesterBear <102464937+ForesterBear@users.noreply.github.com>
2022-06-02 23:02:32 +0300

    d02m06

---
+++ Диплом/Assets/Scripts/Authors.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Authors : MonoBehaviour
+{
+   //private GameObject i;
+
+   void Author()
+    {
+       //if(Input.anyKeyDown)
+      // {
+       //    i.SetActivation = false;
+      // }
+    }
+
+}   
+    
