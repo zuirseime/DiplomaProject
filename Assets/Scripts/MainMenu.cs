5f78ca42ddc272327a786423fb13d549fb01e3b0
zuirseime <vovakibets88@gmail.com>
2024-08-14 11:41:03 +0300

    dramatically changed project structure
    also deleted sounds and menu background

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
+public class MainMenu : MonoBehaviour
+{
+    float volume;
+    public GameObject slider;
+
+    public void Start()
+    {
+        volume = slider.GetComponent<Slider>().value;
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+
+}
